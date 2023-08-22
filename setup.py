8a000be4e70aea87ad0d0328aed8b664677e54a5
Gabriel Gauci Maistre <gabriel@gaucimaistre.com>
2023-08-22 16:30:49 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='For a given city (we will provide lat/lon) will find out 10 most nearby earthquakes (earthquakes that happened in the closest proximity of that city).',
+    author='gabegm',
+    license='',
+)
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import geopandas as gpd
+
+path_to_data = "data/raw/all_month.geojson"
+gdf = gpd.read_file(path_to_data)
+
