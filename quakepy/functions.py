2ff0affe762aae47ec984ccdfbd403adf8023aca
Gabriel Gauci Maistre <gabriel@gaucimaistre.com>
2023-08-23 22:17:08 +0200

    fixed projections and added logs

--- quakepy/functions.py
+++ quakepy/functions.py
@@ -1,49 +1,62 @@
+import logging
+from typing import List
+
 import pandas as pd
 import geopandas as gpd
-from geopandas import GeoDataFrame
+from geopandas import GeoSeries, GeoDataFrame
 from pandas import Series, DataFrame
-from shapely import Point
+from shapely import Point, wkb
+from pyproj import Transformer
 
 import requests
 from requests.exceptions import HTTPError, Timeout
 
-def pretty_print(df: DataFrame) -> str:
-    x = df["title"].to_list()
-    y = df["distance"].to_list()
+logging.basicConfig(level=logging.INFO)
 
-    return lambda x, y: str(x) + " || " + str(y)
+def pretty_print(df: DataFrame) -> str:
+    x_title = df["title"].to_list()
+    y_distance = df["distance"].to_list()
+
+    return ''.join([str(x) + " || " + str(y) + '\n' for x,y in zip(x_title, y_distance)])
 
 def calc_curve_distance(gdf: GeoDataFrame, p: Point) -> Series:
-    s = gdf.distance(p).sort_values(ascending=False)
-    s.name = "distance"
+    logging.info("Reprojecting geometry")
+    gdf = gdf.to_crs(epsg=32663)
 
-    return s
+    transformer = Transformer.from_crs("EPSG:4979", "EPSG:32663")
+    p = Point(transformer.transform(p.x, p.y))
 
-def get_closest_n(s: Series, n: int) -> Series:
-    s = s.sort_values(ascending=False)
+    s_distance = gdf.distance(p).divide(1000).round(0).astype(int)
+    s_distance.name = "distance"
 
-    s = s[:n]
+    return s_distance
 
-    return s
+def get_closest_n(df: DataFrame, n: int) -> DataFrame:
+    return df.sort_values(by="distance", ascending=True)[:n]
 
-def get_joined_data(gs_title: Series, gs_distance: Series) -> DataFrame:
-    df = pd.merge(gs_title, gs_distance, left_index=True, right_index=True)
-
-    return df
-
-def get_data() -> GeoDataFrame:
+def get_data(columns: List[str]) -> GeoDataFrame:
     # path_to_data = "data/raw/all_month.geojson"
     # gdf = gpd.read_file(path_to_data)
     url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
     response = call_api(url)
 
-    gdf = gpd.read_file(response)
+    gdf = gpd.read_file(response)[columns]
 
-    return gdf
+    logging.info(f"Finished processing {len(gdf)} rows")
+
+    _drop_z = lambda geom: wkb.loads(wkb.dumps(geom, output_dimension=2))
+    gdf.geometry = gdf.geometry.transform(_drop_z)
+
+    gdf_depublicated = gdf.drop_duplicates(subset=["geometry"])
+
+    logging.info(f"Depublicated {len(gdf) - len(gdf_depublicated)} rows")
+
+    return gdf_depublicated
 
 def call_api(url:str) -> str:
     try:
+        logging.info(f"GET {url}")
         response = requests.get(
             url,
             params={'q': 'requests+language:python'},
@@ -53,10 +66,10 @@ def call_api(url:str) -> str:
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except Timeout as http_timeout:
-        print('The request timed out')
+        logging.info("The request timed out (might be retried)")
     except HTTPError as http_err:
-        print(f'HTTP error occurred: {http_err}')
+        logging.info(f"HTTP error occurred: {http_err}")
     except Exception as err:
-        print(f'Other error occurred: {err}')
+        logging.info(f"Other error occurred: {err}")
 
     return response.text
--- quakepy/main.py
+++ quakepy/main.py
@@ -1,6 +1,10 @@
 import argparse
+import logging
+
 from shapely import Point
 from quakepy import functions as func
+
+logging.basicConfig(level=logging.INFO)
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -11,17 +15,39 @@ def init_argparse() -> argparse.ArgumentParser:
         "-v", "--version", action="version",
         version = f"{parser.prog} version 1.0.0"
     )
-    parser.add_argument("lat", nargs=1)
-    parser.add_argument("lon", nargs=1)
+    parser.add_argument("lat", type=float_range(-90, 91), nargs=1)
+    parser.add_argument("lon", type=float_range(-90, 91), nargs=1)
 
     return parser
 
+def float_range(mini,maxi):
+    """Return function handle of an argument type function for
+       ArgumentParser checking a float range: mini <= arg <= maxi
+         mini - minimum acceptable argument
+         maxi - maximum acceptable argument"""
+
+    # Define the function with default arguments
+    def float_range_checker(arg):
+        """New Type function for argparse - a float within predefined range."""
+
+        try:
+            f = float(arg)
+        except ValueError:
+            raise argparse.ArgumentTypeError("must be a floating point number")
+        if f < mini or f > maxi:
+            raise argparse.ArgumentTypeError("must be in range [" + str(mini) + " .. " + str(maxi)+"]")
+        return f
+
+    # Return function handle to checking function
+    return float_range_checker
+
 def get_top_n(p: Point):
-    gdf = func.get_data()
-    s = func.calc_curve_distance(gdf, p)
-    s_closest = func.get_closest_n(s, 10)
-    df_joined = func.get_joined_data(gdf["title"], s_closest)
-    top_10_str = func.pretty_print(df_joined)
+    columns = ["title", "geometry"]
+    gdf = func.get_data(columns)
+
+    gdf["distance"] = func.calc_curve_distance(gdf, p)
+    df_closest = func.get_closest_n(gdf, 10)
+    top_10_str = func.pretty_print(df_closest)
 
     return top_10_str
 
@@ -29,9 +55,7 @@ def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
 
-    while (not args.lat or not args.lon):
-        print("Please enter two coordinates.")
-        args = parser.parse_args()
+    logging.info(f"Received coordinates: {args.lat[0]}, {args.lon[0]}")
 
     p = Point(args.lat, args.lon)
 
