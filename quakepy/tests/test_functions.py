69dfc128df3f68ea94b1e770b8afe7a61d544ae0
Gabriel Gauci Maistre <gabriel@gaucimaistre.com>
2023-08-26 13:37:48 +0200

    cleanup

--- quakepy/tests/test_functions.py
+++ quakepy/tests/test_functions.py
@@ -1,33 +1,34 @@
+import unittest
+
 from shapely import Point
 from quakepy import functions as func
 import geopandas as gpd
 
-def test_calc_curve_distance():
-    p = Point(40.730610, -73.935242)
-    gdf = gpd.read_file("data/raw/all_month.geojson")
+class TestSum(unittest.TestCase):
+    def test_calc_curve_distance(self):
+        p = Point(40.730610, -73.935242)
+        gdf = gpd.read_file("data/raw/all_month.geojson")
 
-    dist = func.calc_curve_distance(gdf, p)
+        gdf["dist"] = func.calc_curve_distance(gdf, p)
 
-    assert len(gdf) == len(gdf)
-    assert dist.dtype == int
-    assert gdf.crs.to_epsg() == 32663
-    assert dist.name == "distance"
+        assert len(gdf) == len(gdf)
+        assert gdf["dist"].dtype == int
 
-def test_get_closest_n():
-    n = 5
-    p = Point(40.730610, -73.935242)
+    def test_get_closest_n(self):
+        n = 5
+        p = Point(40.730610, -73.935242)
 
-    gdf = gpd.read_file("data/raw/all_month.geojson")
-    gdf["distance"] = func.calc_curve_distance(gdf, p)
-    gdf_n = func.get_closest_n(gdf, n)
+        gdf = gpd.read_file("data/raw/all_month.geojson")
+        gdf["distance"] = func.calc_curve_distance(gdf, p)
+        gdf_n = func.get_closest_n(gdf, n)
 
-    assert len(gdf_n) == n
-    assert int(gdf_n["distance"][:1]) < int(gdf_n["distance"][-1:])
+        assert len(gdf_n) == n
+        assert int(gdf_n["distance"][:1]) < int(gdf_n["distance"][-1:])
 
-def test_get_data():
-    columns = ["title", "geometry"]
-    gdf = func.get_data(columns)
-    n_duplicates = gdf.drop_duplicates(subset=["geometry"])
+    def test_get_data(self):
+        columns = ["title", "geometry"]
+        gdf = func.get_data(columns)
+        n_duplicates = gdf.drop_duplicates(subset=["geometry"])
 
-    assert gdf["geometry"].z.isna().all()
-    assert len(n_duplicates) == len(gdf)
+        assert gdf["geometry"].z.isna().all()
+        assert len(n_duplicates) == len(gdf)
