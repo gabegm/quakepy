e88c0c4ab8edd7952a1ecc40005e89577dfb7446
Gabriel Gauci Maistre <gabriel@gaucimaistre.com>
2023-08-28 11:11:37 +0200

    added test case

--- quakepy/tests/test_functions.py
+++ quakepy/tests/test_functions.py
@@ -9,10 +9,10 @@ class TestSum(unittest.TestCase):
         p = Point(40.730610, -73.935242)
         gdf = gpd.read_file("data/raw/all_month.geojson")
 
-        gdf["dist"] = func.calc_curve_distance(gdf, p)
+        gdf["distance"] = func.calc_curve_distance(gdf, p)
 
         assert len(gdf) == len(gdf)
-        assert gdf["dist"].dtype == int
+        assert gdf["distance"].dtype == int
 
     def test_get_closest_n(self):
         n = 5
@@ -32,3 +32,19 @@ class TestSum(unittest.TestCase):
 
         assert gdf["geometry"].z.isna().all()
         assert len(n_duplicates) == len(gdf)
+
+    def test_pretty_print(self):
+        p = Point(40.730610, -73.935242)
+        gdf_one = gpd.read_file("data/raw/all_month.geojson")
+        gdf_two = gpd.read_file("data/raw/all_month.geojson")
+
+        gdf_one["distance"] = func.calc_curve_distance(gdf_one, p)
+        gdf_two["distance"] = func.calc_curve_distance(gdf_two, p)
+
+        df_one_closest = func.get_closest_n(gdf_one, 10)
+        df_two_closest = func.get_closest_n(gdf_two, 10)
+
+        top_10_one_str = func.pretty_print(df_one_closest)
+        top_10_two_str = func.pretty_print(df_two_closest)
+
+        assert top_10_one_str == top_10_two_str
